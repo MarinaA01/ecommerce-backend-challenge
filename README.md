913626fb1869d61b532af5d06d7fb6373df62305
Marina <marinajalonzo1@gmail.com>
2024-03-21 17:54:09 -0500

    Route and Readme updates.

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # ecommerce-backend-challenge
-This project builds a backend to an e-commerce website
+
+## Description
+This project was created to provide an ecommerce backend. This project allowed me to practice with ORMs and Sequelize which was a really interesting concept. 
+
+## Installation
+
+N/A
+
+## Usage
+
+This project uses Node.js and Express.js to create APIs to get all the information to process the data. This project also uses Sequelize and SQL information to provide the data.
+
+## Credits
+The AskBCS team on Slack was extremely helpful on the errors in this project.
+
+## License
+MIT License
--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -21,17 +21,19 @@ router.get('/:id', async (req, res) => {
   // be sure to include its associated Product data
   try {
     const tagData = await Tag.findByPk(req.params.id, {
-      include: [{ model: Product, through: ProductTag, as: 'tag_products' }],
+      include: [{ model: Product, through: ProductTag, as: 'tag_products' }]
     });
     if (!tagData) {
       res.status(404).json({ message: 'No tag found with that id!' });
       return;
     }
     res.status(200).json(tagData);
+  } catch (err) {
+    res.status(500).json(err);
   }
 });
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
   // create a new tag
   try {
     const tagData = await Tag.create(req.body);
@@ -41,7 +43,7 @@ router.post('/', (req, res) => {
   }
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req, res) => {
   // update a tag's name by its `id` value
   try {
     const tagData = await Tag.update(req.body, {
@@ -59,7 +61,7 @@ router.put('/:id', (req, res) => {
   }
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete on tag by its `id` value
   try {
     const tagData = await Tag.destroy({
